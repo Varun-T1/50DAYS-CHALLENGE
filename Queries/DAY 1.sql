aa9940f0d8e771e33da296769816e75d1e0c796b
Varun T <varunsaroja09@gmail.com>
2026-04-03 20:09:56 +0530

    Day 4 updates: Data cleaning and deduplication queries

---
+++ Queries/DAY 1.sql
@@ -0,0 +1,75 @@
+use 50_day_challenge;
+
+-#-- CREATE TABLE department
+
+CREATE TABLE department (
+   ddept_id VARCHAR(60),
+   	dept_name  VARCHAR(60));
+
+# --CREATE TABLE employee
+
+CREATE TABLE employee (
+   emp_id VARCHAR(60),
+    emp_name VARCHAR(60),
+    age VARCHAR(60),
+    city	VARCHAR(60),
+    dept_id	VARCHAR(60),
+    hire_date VARCHAR(60)
+    );
+
+#--CREATE TABLE salaries
+
+CREATE TABLE salaries( 
+    salary_id VARCHAR(60),
+    emp_id VARCHAR(60),
+    salary VARCHAR(60),
+    salary_date VARCHAR(60)
+    );
+
+   
+#--CREATE TABLE attendance
+
+    CREATE TABLE attendance (
+        attendance_id VARCHAR(60),
+        emp_id VARCHAR(60),
+        attendance_date VARCHAR(60),
+        status VARCHAR(60)
+    );
+
+ #---CREATE TABLE performance
+
+ CREATE TABLE performance (
+    emp_id VARCHAR(60),
+    rating_2022 VARCHAR(60),
+    rating_2023 VARCHAR(60),
+    rating_2024 VARCHAR(60)
+    );
+
+
+CREATE TABLE Cleaned_department
+as
+SELECT * FROM department;
+ 
+CREATE TABLE Cleaned_employee
+as
+SELECT * FROM employee;
+
+CREATE TABLE Cleaned_salaries
+ as
+SELECT * FROM salaries;
+  
+CREATE TABLE Cleaned_attendance
+ as
+SELECT * FROM attendance;
+  
+CREATE TABLE Cleaned_performance
+ as
+SELECT * FROM performance;
+
+SELECT * FROM Cleaned_department;
+SELECT * FROM Cleaned_employee;
+SELECT * FROM Cleaned_salaries;
+SELECT * FROM Cleaned_attendance;
+SELECT * FROM Cleaned_performance;
+ 
+ 
